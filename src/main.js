436d0b3d1f9c8a6bb7d4ec1a7815db356a23ac36
Radka Gettová <rgettova@gmail.com>
2017-07-02 21:07:07 +0200

    Donation section skeleton done, animate.css in progress

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import index from '../index.html';
 import bootstrap from 'bootstrap-scss';
 import main from './assets/css/main.sass';
 import bootstrapJS from 'bootstrap-js';
+import animate from 'animate-js';
 // import TxtRotate from './assets/js/writeAnim.js';
