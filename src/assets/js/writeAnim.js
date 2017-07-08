2d32297f7e7cc38fda03c71d5277a136b125a3fc
Ivkaa <dark.wolfie184@gmail.com>
2017-07-08 19:43:59 +0200

    Fix bug in animation

--- src/assets/js/writeAnim.js
+++ src/assets/js/writeAnim.js
@@ -51,7 +51,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML atom = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
 
