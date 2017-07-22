c20634faa189c86fffebcdc408d04c9467e89d9e
Radoslava Gettova <radka.gettova@thinkcreatix.com>
2017-07-22 21:20:14 +0200

    On "Donate us" button page scroll smoothly with animation

--- src/assets/js/navbar-animation.js
+++ src/assets/js/navbar-animation.js
@@ -9,3 +9,13 @@ $( window ).scroll(function() {
   }
 }
 });
+
+function scrollToDonateSection() {
+    $('html, body').animate({
+        scrollTop: $("#support").offset().top
+    }, 1500);
+}
+
+$("#donate").click(scrollToDonateSection);
+
+$("#donate-1").click(scrollToDonateSection);
