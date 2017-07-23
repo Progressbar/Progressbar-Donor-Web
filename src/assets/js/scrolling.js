91d81b0bb1f70d445de511e6dfb611b4f68c21ed
Ivkaa <dark.wolfie184@gmail.com>
2017-07-23 20:48:40 +0200

    Fix not working 'podpor nas' button and refactor

--- src/assets/js/scrolling.js
+++ src/assets/js/scrolling.js
@@ -1,8 +1,13 @@
+'use strict'
 function scrollToDonateSection() {
     $('html, body').animate({
         scrollTop: $("#support").offset().top
     }, 1000, 'swing');
 }
 
-$("#donate").click(scrollToDonateSection);
-$("#donate-1").click(scrollToDonateSection);
+const toScroll = [$("#donate"), $("#donate-1"), $(".main-pic").find(":hidden")]
+// the last select is because jquery does not select hidden slovak language element
+
+toScroll.forEach( e => {
+  e.click(scrollToDonateSection);
+})
