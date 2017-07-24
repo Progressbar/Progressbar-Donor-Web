4181a022c18bf3db56f7ab10415ea01f516d1663
Ivkaa <dark.wolfie184@gmail.com>
2017-07-24 21:08:55 +0200

    Fix blinking when switching languages

--- src/assets/js/lang-switching.js
+++ src/assets/js/lang-switching.js
@@ -1,25 +1,25 @@
 'use strict'
 jQuery.fn.swapWith = function(to) {
-    return this.each(function() {
-        let copy_to = $(to).clone(true);
-        let copy_from = $(this).clone(true);
-        $(to).replaceWith(copy_from);
-        $(this).replaceWith(copy_to);
-    });
+  return this.each(function() {
+    let copy_to = $(to).clone(true);
+    let copy_from = $(this).clone(true);
+    $(to).replaceWith(copy_from);
+    $(this).replaceWith(copy_to);
+  });
 };
 
 $( "#slovak" ).click(function() {
-   $("#slovak").attr({'id':'notactive', 'id': 'slovak'});
-   $(".en").fadeOut('slow', function(){
-     $(".sk").fadeIn('slow');
-   });
+  $("#slovak").attr({'id':'notactive', 'id': 'slovak'});
+  $(".en").fadeOut(500, 'linear', function(){
+    $(".sk").stop().fadeTo('slow', 1);
+  });
   $(this).swapWith("#english");
  });
 
- $( "#english" ).click(function() {
-   $("#english").attr({'id':'notactive', 'id': 'english'})
-   $(".sk").fadeOut('slow', function(){
-     $(".en").fadeIn('slow');
-   });
-    $(this).swapWith("#slovak");
- });
+$( "#english" ).click(function() {
+  $("#english").attr({'id':'notactive', 'id': 'english'})
+  $(".sk").fadeOut(500, 'linear', function(){
+    $(".en").stop().fadeTo('slow', 1);
+  });
+  $(this).swapWith("#slovak");
+});
