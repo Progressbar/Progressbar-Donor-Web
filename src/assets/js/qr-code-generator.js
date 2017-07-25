a0f3a22183ed64e8385c8abd536a44498e783722
Ivkaa <dark.wolfie184@gmail.com>
2017-07-25 09:24:10 +0200

    Add generation of qr code for eth

--- src/assets/js/qr-code-generator.js
+++ src/assets/js/qr-code-generator.js
@@ -13,9 +13,9 @@ $(document).ready(function(){
     });
   }
 
-  function generateUrl(adress, amount, rate) {
+  function generateUrl(cryptoCr, adress, amount, rate) {
     const valueInCrypto = amount * rate;
-    return `https://chart.googleapis.com/chart?cht=qr&chl=bitcoin%3A${adress}%3Famount%3D${valueInCrypto}&choe=UTF-8&chs=230x230`;
+    return `https://chart.googleapis.com/chart?cht=qr&chl=${cryptoCr}%3A${adress}%3Famount%3D${valueInCrypto}&choe=UTF-8&chs=230x230`;
   }
 
   function generateBySquareUrl(amount) {
@@ -35,11 +35,13 @@ $(document).ready(function(){
 
   function setNewQrs(amount) {
     getRates().then(function (rate) {
-      const btcUrl = generateUrl(btcAdress, amount, rate.BTC);
-      const ltcUrl = generateUrl(ltcAdress, amount, rate.LTC);
+      const btcUrl = generateUrl('bitcoin', btcAdress, amount, rate.BTC);
+      const ltcUrl = generateUrl('litecoin', ltcAdress, amount, rate.LTC);
+      const ethUrl = generateUrl('ethereum', ethAdress, amount, rate.ETH);
       const bySquareUrl = generateBySquareUrl(amount);
       $('.btc').attr('src', btcUrl);
       $('.ltc').attr('src', ltcUrl);
+      $('.eth').attr('src', ethUrl);
       $('.paybysquare').attr('src', bySquareUrl);
 
     })
